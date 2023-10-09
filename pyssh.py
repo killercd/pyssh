b871661cce3ca853ac3577dda72f4d4259e5d82e
killercd@gmail.com <killercd@gmail.com>
2023-10-09 22:03:49 +0200

    fast commit

--- pyssh.py
+++ pyssh.py
@@ -4,7 +4,6 @@ import paramiko
 
 
 def r_output(stdout, stderr, line_read=False):
-    
     if not line_read:
         output = stdout.read().decode('utf-8')
         if output:
@@ -18,7 +17,7 @@ def r_output(stdout, stderr, line_read=False):
                 break
             print(line)
 
-def run(host, 
+def run(host,
         user,
         passsword,
         command,
@@ -27,27 +26,24 @@ def run(host,
     """
     run specific command on a remote computer
     """
-    
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(host, username=user, password=passsword)
-    
     stdin, stdout, stderr = ssh_client.exec_command(command)
-    
+
     if not line_output:
         output, status = r_output(stdout, stderr)
         print(output)
     else:
         r_output(stdout, stderr, line_output)
-    
+
     ssh_client.close()
 
-def upload(host, 
+def upload(host,
             user,
             passsword,
             local_file,
             remote_file):
-    
 
     """
     upload a file to a specific folder
@@ -60,19 +56,17 @@ def upload(host,
     sftp.put(local_file, remote_file)
     sftp.close()
     ssh_client.close()
-    
-def download(host, 
+
+def download(host,
             user,
             passsword,
             remote_file,
             local_file):
     """
     download a file to a specific folder
-    
     """
     ssh_client = paramiko.Transport((host, 22))
     ssh_client.connect(username=user, password=passsword)
-    
     sftp = paramiko.SFTPClient.from_transport(ssh_client)
     sftp.get(remote_file, local_file)
     sftp.close()
